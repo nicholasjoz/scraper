09b927da1db4b3d6e6f0d5aa1828ed882c5afbf6
Nicholas Jozefczyk <nickjay@Nicholass-MacBook-Air.local>
2018-10-01 15:59:17 -0700

    back

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-/* mongoose.Promise = Promise; */
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 /* mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true }); */

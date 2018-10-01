9acdf8c300d786ebe8d6ece952fefec8171ca3de
Nicholas Jozefczyk <nickjay@Nicholass-MacBook-Air.local>
2018-10-01 15:57:31 -0700

    another

--- server.js
+++ server.js
@@ -30,14 +30,14 @@ app.use(express.static("public"));
 //mongoose.connect("mongodb://localhost/week18Populater", { useNewUrlParser: true });
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-/* var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"; */
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-/* mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI); */
+/* mongoose.Promise = Promise; */
+mongoose.connect(MONGODB_URI);
 
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+/* mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true }); */
 
 
 // A GET route for scraping the echoJS website
